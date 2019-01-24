cc1193a7347bb11be7134b9b9b5bf4f9226e4c77
JoaoAreias <joaoareiasmoraes@gmail.com>
2019-01-24 12:51:42 -0700

    Changes to be committed: 	new file:   HackerRank/ReverseShuffleMerge.cpp 	modified:   HackerRank/SpecialPalindromeAgain.cpp

---
+++ HackerRank/ReverseShuffleMerge.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the reverseShuffleMerge function below.
+string reverseShuffleMerge(string s) {
+	string out;
+	map<char, int> freq_count;
+	for(const char &c: s)
+		freq_count[c]++;
+
+	/* Output string must have half the frequency 
+	 for each character as the original one */
+	for(auto &p: freq_count)
+		p.second /= 2;
+
+	// Transversing string in reverse
+	for(auto it=s.crbegin(); it != s.crend(); it++){
+		
+	}
+
+	return out;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = reverseShuffleMerge(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
--- HackerRank/SpecialPalindromeAgain.cpp
+++ HackerRank/SpecialPalindromeAgain.cpp
@@ -2,18 +2,26 @@
 
 using namespace std;
 
-bool isPalindrome(string &str){
-    int size = str.size();
-    for(int i=0; i < size/2; i++)
-        if(str[i] != str[size - i - 1])
-            return false;
-    return true;
-}
-
 // Complete the substrCount function below.
 long substrCount(int n, string s) {
-
-
+    long count = n; // Every letter in the string is a special palindrome 
+    for(int i=0; i<n; i++){
+        char first = s[i]; // First letter of the palindrome
+        int diffIdx = -1; // Index of the first different letter of the palindrome
+        for(int j=i+1; j<n; j++){
+            if(s[j] == s[i]){
+                if((diffIdx == -1) || (j - diffIdx == diffIdx - i))
+                    count++;
+            }
+            else{
+                if(diffIdx == -1)
+                    diffIdx = j;
+                else
+                    break;
+            }
+        }
+    }
+    return count;
 }
 
 int main()
