ada1f16a5224cc04eb3447a0cbc011102ebec365
JoaoAreias <joaoareiasmoraes@gmail.com>
2019-01-31 11:44:40 -0700

    new file:   HackerRank/MaxArraySum.cpp

---
+++ HackerRank/MaxArraySum.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the maxSubsetSum function below.
+long int maxSubsetSum(vector<long> &arr) {
+    vector<long int> memo_table(arr.size());
+    int size = arr.size();
+
+    memo_table[0] = arr[0];
+    memo_table[1] = max(arr[0], arr[1]);
+
+    for(int i=2; i < size; i++){
+        memo_table[i] = max(
+            max(arr[i], memo_table[i-1]), 
+            max(arr[i] + memo_table[i-2], memo_table[i-2])
+        );
+    }
+    printf("%ld\n", memo_table[size-1]);
+    return memo_table[size-1];
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<long int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        long int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    long int res = maxSubsetSum(arr);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
